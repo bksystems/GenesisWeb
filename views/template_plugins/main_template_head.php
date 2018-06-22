42967eb8f31095a7ee772d9b064aa1c17af684e0
Juan Carlos Rizo Flores <jcarlos.rizo@gmail.com>
2018-06-22 13:10:44 -0500

    Index actualizado

---
+++ views/template_plugins/main_template_head.php
@@ -0,0 +1,12 @@
+<title><?php echo $title_page; ?> </title>
+<link rel="stylesheet" type="text/css" href="content/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="content/css/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
+<script type="text/javascript" src="content/js/jquery-3.3.1.min.js"></script>
+<script type="text/javascript" src="content/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="content/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="content/js/dataTables.bootstrap4.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+<script type="text/javascript" src="content/js/morris.min.js"></script>
+<script type="text/javascript" src="content/js/functions_genesis_structure.js"></script>
+<script type="text/javascript" src="content/js/dashboard_graphs.js"></script>
